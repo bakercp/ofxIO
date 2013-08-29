bc8f958cb9dfd4e9ebe50d1ecb5bd98f26b46942
Christopher Baker <me@christopherbaker.net>
2013-08-29 16:39:39 -0500

    Added config.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxIO
+	ADDON_DESCRIPTION = A collection of I/O core utils.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "directories" "io" "file filter"
+	ADDON_URL = http://github.com/bakercp/ofxIO
