2f1266f67f694cbf5ff39972c922f5cc4156ecb6
Christopher Baker <me@christopherbaker.net>
2013-12-21 14:48:36 -0600

    Added documentation.

--- libs/ofxIO/include/ofx/IO/AbstractTypes.h
+++ libs/ofxIO/include/ofx/IO/AbstractTypes.h
@@ -42,70 +42,134 @@ class ByteBuffer;
 
 
 class AbstractByteSource
+    /// \brief   Represents the abstract notion of a byte source.
+    /// \details An AbstractByteSource has bytes that can be read.
 {
 public:
-    AbstractByteSource()
-    {
-    }
-
     virtual ~AbstractByteSource()
+        ///< Destroys the byte source.
     {
     }
 
     virtual std::size_t readBytes(uint8_t* buffer, std::size_t size) const = 0;
+        ///< \brief Read bytes from this source into a target buffer.
+        ///< \param buffer is the target buffer to be filled with bytes.
+        ///< \param size is the number of bytes to be written into the target buffer.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the target buffer.
+
     virtual std::size_t readBytes(std::vector<uint8_t>& buffer) const = 0;
+        ///< \brief Read bytes from this source into a target buffer.
+        ///< \param buffer is the target buffer to be filled with bytes.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the target buffer.
+
     virtual std::size_t readBytes(std::string& buffer) const = 0;
+        ///< \brief Read bytes from this source into a target buffer.
+        ///< \param buffer is the target buffer to be filled with bytes.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the target buffer.
+
     virtual std::size_t readBytes(ByteBuffer& buffer) const = 0;
+        ///< \brief Read bytes from this source into a target buffer.
+        ///< \param buffer is the target buffer to be filled with bytes.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the target buffer.
 
 };
 
 
 
 class AbstractByteSink
+    /// \brief Represents the abstract notion of a byte sink.
+    /// \details An AbstractByteSink can receive bytes written
+    /// from various sources.
 {
 public:
-    AbstractByteSink()
-    {
-    }
-
     virtual ~AbstractByteSink()
+        ///< Destroys the byte sink.
     {
     }
 
     virtual std::size_t writeByte(uint8_t data) = 0;
+        ///< \brief Write a single byte into this byte sink.
+        ///< \param data is the single byte to be written into this byte sink.
+        ///< \returns the number of bytes that were successfully
+        ///< written into this byte sink.
+
     virtual std::size_t writeBytes(const uint8_t* buffer, std::size_t size) = 0;
+        ///< \brief Write the contents of the buffer into this byte sink.
+        ///< \param buffer is the array of bytes to be written to this byte sink.
+        ///< \param size is the number of bytes in the buffer.
+        ///< \returns the number of bytes that were successfully
+        ///< written into this byte sink.
+
     virtual std::size_t writeBytes(const std::vector<uint8_t>& buffer) = 0;
+        ///< \brief Write the contents of the buffer into this byte sink.
+        ///< \param buffer is the array of bytes to be written to this byte sink.
+        ///< \returns the number of bytes that were successfully
+        ///< written into this byte sink.
+
     virtual std::size_t writeBytes(const std::string& buffer) = 0;
+        ///< \brief Write the contents of the buffer into this byte sink.
+        ///< \param buffer is the array of bytes to be written to this byte sink.
+        ///< \returns the number of bytes that were successfully
+        ///< written into this byte sink.
 
 };
 
 
 class AbstractBufferedByteSource
+    /// \brief Represents the abstract notion a buffered byte source.
+    /// \details Unlike a AbstractByteSource, a buffered byte source is
+    /// consumed as it is read.
 {
 public:
-    AbstractBufferedByteSource()
-    {
-    }
-    
     virtual ~AbstractBufferedByteSource()
+        ///< Destroys the buffered byte source.
     {
     }
 
     virtual std::size_t readByte(uint8_t& data) = 0;
+        ///< \brief Read a single byte from this source into the referenced variable.
+        ///< \param data is referenced variable to be filled.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the referenced data variable.  The maxium
+        ///< return value is 1.
+
     virtual std::size_t readBytes(uint8_t* buffer, std::size_t size) = 0;
+        ///< \brief Read bytes from this source into a target buffer.
+        ///< \param buffer is the target buffer to be filled with bytes.
+        ///< \param size is the number of bytes to be written into the target buffer.
+        ///< \returns the number of bytes that were successfully
+        ///< written into the target buffer.
 
     virtual std::size_t available() const = 0;
+        ///< \brief Query the number of buffered bytes.
+        ///< \returns the number of bytes available to be read.
     
 };
 
 
-class AbstractFileFilter {
+class AbstractFileFilter
+    /// \brief Represents the abstract notion of a file filter.
+    /// \details Subclasses implementing this interface can accept files
+    /// based on a criteria defined in the accept() method.
+{
 public:
     virtual ~AbstractFileFilter()
+        ///< Destroys the file filter.
     {
     }
     
     virtual bool accept(const Poco::File& file) const = 0;
+        ///< \brief Accept a file based on a defined criteria.
+        ///< \returns true iff the file is accepted by the file filter.
+
+    
+};
+
+
 class AbstractSearchPath
     /// \brief Represents the abstract notion of a search path.
 {
