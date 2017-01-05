7aaf81e2ef1094c5182194826f8ed8f303e70dcb
Christopher Baker <me@christopherbaker.net>
2017-01-05 16:03:38 -0600

    Update addons_config.mk.

--- addon_config.mk
+++ addon_config.mk
@@ -2,7 +2,12 @@ meta:
 	ADDON_NAME = ofxIO
 	ADDON_DESCRIPTION = A collection of I/O core utils.
 	ADDON_AUTHOR = bakercp
-	ADDON_TAGS = "directories" "io" "file filter"
+	ADDON_MAINTAINER = bakercp
+	ADDON_TAGS = "directories" "io" "file filter" "compression" "encoding"
 	ADDON_URL = http://github.com/bakercp/ofxIO
+	ADDON_PLATFORMS = android emscripten ios linux linuxarmv6 linuxarmv7 msys2 osx vs
+	ADDON_DOCUMENTATION = http://bakercp.github.io/ofxIO/
+	ADDON_VERSION = develop
+	ADDON_CATEGORY = utilities
 common:
 	ADDON_DEPENDENCIES = ofxPoco
