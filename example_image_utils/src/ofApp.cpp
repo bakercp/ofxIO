d30e34babced4b2fb348c8775e8225d35c1a0add
Christopher Baker <me@christopherbaker.net>
2017-03-21 07:27:11 -0500

    Add examples.

---
+++ example_image_utils/src/ofApp.cpp
@@ -0,0 +1,43 @@
+//
+// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#include "ofApp.h"
+
+
+void ofApp::setup()
+{
+    std::string path = ofToDataPath("openFrameworks.png", true);
+
+    ofx::IO::ByteBuffer byteBuffer;
+
+    try
+    {
+        std::streamsize in = ofx::IO::ByteBufferUtils::loadFromFile(path, byteBuffer);
+
+        ofLogNotice("ofApp::setup") << in << " bytes read.";
+
+        std::string newPath = ofToDataPath("openFrameworks_copy.png", true);
+
+        bool result = ofx::IO::ByteBufferUtils::saveToFile(byteBuffer, newPath);
+
+        if (result)
+        {
+            ofLogNotice("ofApp::setup") << "File written to " << newPath;
+        }
+        else
+        {
+            ofLogError("ofApp::setup") << "File not written bytes written.";
+        }
+    }
+    catch (Poco::Exception& exception)
+    {
+        ofLogError("ofApp::setup") << "Exception: " << exception.displayText();
+    }
+
+    ofExit();
+
+}
---
+++ example_json_utils/src/ofApp.h
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#pragma once
+
+
+#include "ofMain.h"
+#include "ofxIO.h"
+
+
+class ofApp: public ofBaseApp
+{
+public:
+    void setup() override;
+
+};
