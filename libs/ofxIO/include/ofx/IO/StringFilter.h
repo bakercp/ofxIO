a706dc56a6972b9cab85dc71a8b679717dc7657d
Christopher Baker <me@christopherbaker.net>
2019-05-29 18:57:19 -0600

    StringFilter enhancements, remove regex.

--- libs/ofxIO/include/ofx/IO/StringFilter.h
+++ libs/ofxIO/include/ofx/IO/StringFilter.h
@@ -10,7 +10,8 @@
 
 #include <set>
 #include <string>
-#include <json.hpp>
+#include <unordered_set>
+#include "json.hpp"
 
 
 namespace ofx {
@@ -24,20 +25,18 @@ namespace IO {
 class StringFilter
 {
 public:
-    enum ReplacementType
-    {
-        ALL,
-        CONSONANTS,
-        VOWELS
-    };
+    struct Settings;
 
     StringFilter();
+    StringFilter(const Settings& settings);
     ~StringFilter();
 
-    /// \brief Replace
-    std::string filter(const std::string& input,
-                       char32_t replacement = '*',
-                       bool ignoreCase = true) const;
+    void setup(const Settings& settings);
+
+    /// \returns the Settings.
+    Settings settings() const;
+
+    std::string filter(const std::string& input) const;
 
     std::vector<std::string> getBlacklist() const;
     void setBlacklist(const std::vector<std::string>& list);
@@ -45,11 +44,14 @@ public:
     std::vector<std::string> getWhitelist() const;
     void setWhitelist(const std::vector<std::string>& list);
 
-    std::vector<std::string> getBlacklistRegex() const;
-    void setBlacklistRegex(const std::vector<std::string>& list);
+    struct Settings
+    {
+        char replacement = DEFAULT_REPLACEMENT;
+        bool ignoreCase = true;
 
-    std::vector<std::string> getWhitelistRegex() const;
-    void setWhitelistRegex(const std::vector<std::string>& list);
+        const static char DEFAULT_REPLACEMENT;
+    };
+
 
 private:
     /// \brief List of blacklisted strings in UTF8 format.
@@ -58,12 +60,6 @@ private:
     /// \brief List of whitelisted strings in UTF8 format.
     std::vector<std::string> _whitelist;
 
-    /// \brief List of blacklisted regex patterns in UTF8 format.
-    std::vector<std::string> _blackListRegex;
-
-    /// \brief List of whitelisted regex patterns in UTF8 format.
-    std::vector<std::string> _whitelistRegex;
-
     /// \brief True if lists need to be rebuilt.
     mutable bool _listDirty = true;
 
@@ -71,30 +67,46 @@ private:
     void _rebuildLists() const;
 
     /// \brief A list of blacklisted words minus whitelisted words.
-    mutable std::vector<std::string> _list;
+    mutable std::vector<std::string> _wordList;
 
-    /// \brief A list of blacklisted regex patterns minus whitelisted regex patterns.
-    mutable std::vector<std::string> _listRegex;
+    mutable std::unordered_set<std::string> _wordSet;
 
+    /// \brief A list of blacklisted phrases minutes whitelisted phrases.
+    mutable std::vector<std::string> _phraseList;
+
+    /// \brief The current settings.
+    Settings _settings;
 };
 
 
 
 inline void to_json(nlohmann::json& j, const StringFilter& v)
 {
+    j["settings"]["replacement"] = v.settings().replacement;
+    j["settings"]["ignore_case"] = v.settings().ignoreCase;
+
+
     j["blacklist"] = v.getBlacklist();
-    j["blacklist_regex"] = v.getBlacklistRegex();
     j["whitelist"] = v.getWhitelist();
-    j["whitelist_regex"] = v.getWhitelistRegex();
 }
 
 
 inline void from_json(const nlohmann::json& j, StringFilter& v)
 {
     v.setBlacklist(j.value("blacklist", std::vector<std::string>()));
-    v.setBlacklistRegex(j.value("blacklist_regex", std::vector<std::string>()));
     v.setWhitelist(j.value("whitelist", std::vector<std::string>()));
-    v.setWhitelistRegex(j.value("whitelist_regex", std::vector<std::string>()));
+
+    nlohmann::json settings = j.value("settings", nlohmann::json());
+
+    StringFilter::Settings s;
+
+    if (!settings.is_null())
+    {
+        int c = settings.value("replacement", int(StringFilter::Settings::DEFAULT_REPLACEMENT));
+        s.replacement = char(c);
+        s.ignoreCase = settings.value("ignore_case", true);
+        v.setup(s);
+    }
 }
 
 
--- libs/ofxIO/src/StringFilter.cpp
+++ libs/ofxIO/src/StringFilter.cpp
@@ -6,7 +6,6 @@
 
 
 #include "ofx/IO/StringFilter.h"
-#include <regex>
 #include "ofUtils.h"
 
 
@@ -14,7 +13,17 @@ namespace ofx {
 namespace IO {
 
 
+const char StringFilter::Settings::DEFAULT_REPLACEMENT = '*';
+
+
 StringFilter::StringFilter()
+{
+}
+
+
+StringFilter::StringFilter(const Settings& settings):
+    _listDirty(true),
+    _settings(settings)
 {
 }
 
@@ -24,37 +33,117 @@ StringFilter::~StringFilter()
 }
 
 
-std::string StringFilter::filter(const std::string& _input,
-                                 char32_t replacementChar,
-                                 bool ignoreCase) const
+void StringFilter::setup(const Settings& settings)
+{
+    _listDirty = true;
+    _settings = settings;
+}
+
+
+StringFilter::Settings StringFilter::settings() const
+{
+    return _settings;
+}
+
+    
+std::string StringFilter::filter(const std::string& _input) const
 {
     _rebuildLists();
 
+    std::string output = _input;
     std::string input = _input;
 
-    for (auto& needle: _list)
-    {
-        std::regex_constants::syntax_option_type flags = std::regex_constants::ECMAScript;
-        if (ignoreCase)
-            flags |= std::regex_constants::icase;
+    if (_settings.ignoreCase)
+        std::transform(input.begin(),
+                       input.end(),
+                       input.begin(),
+                       ::tolower);
 
-        std::regex regexNeedle(needle, flags);
-        std::string replacement(ofUTF8Length(needle), replacementChar);
-        input = std::regex_replace(input, regexNeedle, replacement);
+    // Remove whole phrases.
+    for (auto needle: _phraseList)
+    {
+        if (needle.size() <= input.size())
+        {
+            std::size_t pos = input.find(needle);
+            while (pos != std::string::npos)
+            {
+                std::string replacement = needle;
+
+                std::transform(replacement.begin(),
+                               replacement.end(),
+                               replacement.begin(),
+                               [&](unsigned char c) -> unsigned char
+                {
+                    return std::isspace(c) ? c : _settings.replacement;
+                });
+
+                output.replace(pos, needle.size(), replacement);
+                pos = input.substr(pos + needle.size()).find(needle);
+            }
+        }
     }
 
-    for (auto& needle: _listRegex)
-    {
-        std::regex_constants::syntax_option_type flags = std::regex_constants::ECMAScript;
-        if (ignoreCase)
-            flags |= std::regex_constants::icase;
+    // Remove individual words.
+    std::string result;
+    std::size_t currentWordStartIndex = std::numeric_limits<std::size_t>::max();
+    std::size_t lastDelimiter = std::numeric_limits<std::size_t>::max();
 
-        std::regex regexNeedle(needle, flags);
-        std::string replacement(ofUTF8Length(needle), replacementChar);
-        input = std::regex_replace(input, regexNeedle, replacement);
+    for (std::size_t i = 0; i < input.size(); ++i)
+    {
+        bool isDelimiter = std::isspace(input[i])
+                        || std::ispunct(input[i]);
+
+        bool wordEnd = false;
+
+        if (i == 0)
+        {
+            if (isDelimiter)
+                lastDelimiter = i;
+            else
+                currentWordStartIndex = i;
+        }
+        else if (isDelimiter)
+        {
+            if (lastDelimiter != i - 1)
+                wordEnd = true;
+
+            lastDelimiter = i;
+        }
+        else // not a delimiter or i == 0
+        {
+            if (lastDelimiter == i - 1)
+                currentWordStartIndex = i;
+            else if (i == input.size() - 1)
+                wordEnd = true;
+        }
+
+        if (wordEnd)
+        {
+            std::size_t size = i - currentWordStartIndex;
+            std::string word = input.substr(currentWordStartIndex, size);
+
+            for (auto& needle: _wordList)
+            {
+                if (needle.size() == size)
+                {
+                    if (_settings.ignoreCase)
+                        std::transform(word.begin(),
+                                       word.end(),
+                                       word.begin(),
+                                       ::tolower);
+
+                    if (_wordSet.find(word) != _wordSet.end())
+                    {
+                        std::string replacement(size, _settings.replacement);
+                        output.replace(currentWordStartIndex, size, replacement);
+                    }
+                }
+                if (needle.size() < size) break;
+            }
+        }
     }
 
-    return input;
+    return output;
 }
 
 
@@ -84,55 +173,70 @@ void StringFilter::setWhitelist(const std::vector<std::string>& list)
 }
 
 
-std::vector<std::string> StringFilter::getBlacklistRegex() const
-{
-    return _blackListRegex;
-}
-
-
-void StringFilter::setBlacklistRegex(const std::vector<std::string>& list)
-{
-    _blackListRegex = list;
-    _listDirty = true;
-}
-
-
-std::vector<std::string> StringFilter::getWhitelistRegex() const
-{
-    return _whitelistRegex;
-}
-
-
-void StringFilter::setWhitelistRegex(const std::vector<std::string>& list)
-{
-    _whitelistRegex = list;
-    _listDirty = true;
-}
-
-
 void StringFilter::_rebuildLists() const
 {
+    auto hasWhitespace = [](const std::string& input) -> bool
+    {
+        for (auto& c: input)
+            if (std::isspace(c))
+                return true;
+
+        return false;
+    };
+
     if (_listDirty)
     {
-        _list = _blackList;
-        for (auto& item: _whitelist)
-            _list.erase(std::remove(_list.begin(), _list.end(), item), _list.end());
+        _wordList.clear();
+        _phraseList.clear();
+
+        for (auto item: _blackList)
+        {
+            if (_settings.ignoreCase)
+                std::transform(item.begin(),
+                               item.end(),
+                               item.begin(),
+                               ::tolower);
+
+            if (hasWhitespace(item))
+                _phraseList.push_back(item);
+            else _wordList.push_back(item);
+        }
+
+        for (auto item: _whitelist)
+        {
+            if (_settings.ignoreCase)
+                std::transform(item.begin(),
+                               item.end(),
+                               item.begin(),
+                               ::tolower);
+
+            if (hasWhitespace(item))
+                _phraseList.erase(std::remove(_phraseList.begin(),
+                                              _phraseList.end(),
+                                              item),
+                                  _phraseList.end());
+            else
+                _wordList.erase(std::remove(_wordList.begin(),
+                                            _wordList.end(),
+                                            item),
+                                _wordList.end());
+        }
+
+        auto sortBySize = [](const std::string& a,
+                             const std::string& b) -> bool
+        {
+            return a.size() > b.size();
+        };
 
         // Order list by length to make sure smaller versions of words do not occur first.
-        std::sort(_list.begin(), _list.end(), [](const std::string& a, const std::string& b) { return a.size() > b.size(); });
+        std::sort(_phraseList.begin(), _phraseList.end(), sortBySize);
+        std::sort(_wordList.begin(), _wordList.end(), sortBySize);
 
-        _listRegex = _blackListRegex;
-        for (auto& item: _whitelistRegex)
-            _listRegex.erase(std::remove(_listRegex.begin(), _listRegex.end(), item), _listRegex.end());
-
-        // Order list by length to make sure smaller versions of words do not occur first.
-        std::sort(_listRegex.begin(), _listRegex.end(), [](const std::string& a, const std::string& b) { return a.size() > b.size(); });
+        _wordSet = std::unordered_set<std::string>(_wordList.begin(), _wordList.end());
 
         _listDirty = false;
     }
 }
-
-
 
 
 } }  // namespace ofx::IO
